2a7a4593ec6178205ca5bc567d37d5fed7f5decc
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2020-04-22 18:40:19 +0000

    kWorker: Fixed bad paMemMappings realloc call. (Caused trouble with moc.exe as it is doing lots of memory mappings.)
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3338 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kWorker/kWorker.c
+++ src/kWorker/kWorker.c
@@ -7647,7 +7647,7 @@ static PVOID WINAPI kwSandbox_Kernel32_MapViewOfFile(HANDLE hSection, DWORD dwDe
                     cNew *= 2;
                 else
                     cNew = 32;
-                pvNew = kHlpRealloc(g_Sandbox.paMemMappings, cNew * sizeof(g_Sandbox.paMemMappings));
+                pvNew = kHlpRealloc(g_Sandbox.paMemMappings, cNew * sizeof(g_Sandbox.paMemMappings[0]));
                 if (pvNew)
                     g_Sandbox.paMemMappings = (PKWMEMMAPPING)pvNew;
                 else
