39c8caefeec16011d0273647849857057f0c7c2d
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2022-01-29 02:39:47 +0000

    lib: Changed the console-optimization wrappers to use the get_crt_codepage() function as that's more appropriate than GetConsoleCP() for use with _cputws(). Also made them avoid the heap for smaller writes that could fit into a reasonable (2KiB) stack buffer.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3547 8c291193-6614-0410-a08d-adc6e07170bb

--- src/lib/console.h
+++ src/lib/console.h
@@ -40,6 +40,9 @@ typedef intptr_t ssize_t;
 #else
 # include <unistd.h>
 #endif
+#ifdef KBUILD_OS_WINDOWS
+# include "get_codepage.h"
+#endif
 
 
 #ifdef KBUILD_OS_WINDOWS
@@ -48,6 +51,5 @@ extern int      is_console_handle(intptr_t hHandle);
 extern int      is_console(int fd);
 extern ssize_t  maybe_con_write(int fd, void const *pvBuf, size_t cbToWrite);
 extern size_t   maybe_con_fwrite(void const *pvBuf, size_t cbUnit, size_t cUnits, FILE *pFile);
-
 #endif
 
