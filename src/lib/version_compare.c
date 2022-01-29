0d0fab2b30d1a3f550892655115ad75006d4b32d
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2022-01-29 02:57:33 +0000

    lib/version_compare.c: Corrected sorting of 'vcc/v14.2/' and 'vcc/v14.2.11.9/' so that the former is sorted before the latter, as one would expect when soring a $(wildcard ) listing.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3551 8c291193-6614-0410-a08d-adc6e07170bb

--- src/lib/version_compare.c
+++ src/lib/version_compare.c
@@ -115,9 +115,9 @@ static int compare_failed(char ch1, char ch2)
         return -1;
     if (ch2 == '~')
         return 1;
-    if (ch1 == '\0')
+    if (ch1 == '\0' || ch1 == '/') /* treat '/' similar to '\0' to deal with the v14.2/ vs v14.2.11.9/ case.  */
         return -1;
-    if (ch2 == '\0')
+    if (ch2 == '\0' || ch2 == '/')
         return 1;
     if (isdigit(ch1))
         return -1;
@@ -250,6 +250,10 @@ int main()
         { +1, "kBuild-0.099", "kBuild-0.99~" },
         { +1, "1.2.3r4567890", "1.2.3rc1" },
         { +1, "1.2.3r4567890", "1.2.3RC1" },
+        { -1, "/tools/win.amd64/vcc/v14.2/Tools", "/tools/win.amd64/vcc/v14.2.11.9/Tools" },
+        { -1, "/tools/win.amd64/vcc/v14.2/Tools", "/tools/win.amd64/vcc/v14.211.9/Tools" },
+        { -1, "/tools/win.amd64/vcc/v14.2/Tools", "/tools/win.amd64/vcc/v14.2r2/Tools" },
+        { -1, "/tools/win.amd64/vcc/v14.2/Tools", "/tools/win.amd64/vcc/v14.2-r2/Tools" },
     };
     unsigned cErrors = 0;
     unsigned i;
