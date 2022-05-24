b00158d048c90897713bb9d929e413f8b49ffffa
bird <bird@8c291193-6614-0410-a08d-adc6e07170bb>
2022-05-24 20:40:24 +0000

    kmk/incdep.c: Fixed buffer/heap trashing in incdep_unescape_and_cache_filename when encountering non-escaping slashes.
    git-svn-id: http://svn.netlabs.org/repos/kbuild/trunk@3565 8c291193-6614-0410-a08d-adc6e07170bb

--- src/kmk/incdep.c
+++ src/kmk/incdep.c
@@ -1403,8 +1403,8 @@ incdep_unescape_and_cache_filename(struct incdep *curdep, char *start, const cha
               size_t slashes = 1;
               while (ch2 == '\\')
                 {
-                  ch2 = *src++;
                   slashes++;
+                  ch2 = *src++;
                 }
 
               /* Is it escapable? */
@@ -1536,10 +1536,9 @@ incdep_unescape_and_cache_filename(struct incdep *curdep, char *start, const cha
               /* Just output the slash if non-escapable character: */
               else
                 {
-                  src--;
                   while (slashes-- > 0)
                     *dst++ = '\\';
-                  *dst++ = ch;
+                  src--;
                 }
             }
         }
